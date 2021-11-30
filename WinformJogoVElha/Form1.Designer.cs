e7f13c3f7d008d8a1d59320ade5f8efafb3f1a5a
Nicolas <Nicolas@DESKTOP-QI0UNP7>
2021-11-30 19:09:43 -0300

    Adicionar arquivos de projeto.

---
+++ WinformJogoVElha/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace WinformJogoVElha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnc1l1 = new System.Windows.Forms.Button();
+            this.btnc1l2 = new System.Windows.Forms.Button();
+            this.btnc1l3 = new System.Windows.Forms.Button();
+            this.btnc2l3 = new System.Windows.Forms.Button();
+            this.btnc2l2 = new System.Windows.Forms.Button();
+            this.btnc2l1 = new System.Windows.Forms.Button();
+            this.btnc3l3 = new System.Windows.Forms.Button();
+            this.btnc3l2 = new System.Windows.Forms.Button();
+            this.btnc3l1 = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVez = new System.Windows.Forms.Label();
+            this.btnRecomecar = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblVencedor = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnc1l1
+            // 
+            this.btnc1l1.Enabled = false;
+            this.btnc1l1.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc1l1.Location = new System.Drawing.Point(149, 89);
+            this.btnc1l1.Name = "btnc1l1";
+            this.btnc1l1.Size = new System.Drawing.Size(90, 45);
+            this.btnc1l1.TabIndex = 0;
+            this.btnc1l1.UseVisualStyleBackColor = true;
+            this.btnc1l1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc1l2
+            // 
+            this.btnc1l2.Enabled = false;
+            this.btnc1l2.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc1l2.Location = new System.Drawing.Point(149, 160);
+            this.btnc1l2.Name = "btnc1l2";
+            this.btnc1l2.Size = new System.Drawing.Size(90, 45);
+            this.btnc1l2.TabIndex = 1;
+            this.btnc1l2.UseVisualStyleBackColor = true;
+            this.btnc1l2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc1l3
+            // 
+            this.btnc1l3.Enabled = false;
+            this.btnc1l3.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc1l3.Location = new System.Drawing.Point(149, 228);
+            this.btnc1l3.Name = "btnc1l3";
+            this.btnc1l3.Size = new System.Drawing.Size(90, 45);
+            this.btnc1l3.TabIndex = 2;
+            this.btnc1l3.UseVisualStyleBackColor = true;
+            this.btnc1l3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc2l3
+            // 
+            this.btnc2l3.Enabled = false;
+            this.btnc2l3.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc2l3.Location = new System.Drawing.Point(267, 228);
+            this.btnc2l3.Name = "btnc2l3";
+            this.btnc2l3.Size = new System.Drawing.Size(90, 45);
+            this.btnc2l3.TabIndex = 3;
+            this.btnc2l3.UseVisualStyleBackColor = true;
+            this.btnc2l3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc2l2
+            // 
+            this.btnc2l2.Enabled = false;
+            this.btnc2l2.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc2l2.Location = new System.Drawing.Point(267, 160);
+            this.btnc2l2.Name = "btnc2l2";
+            this.btnc2l2.Size = new System.Drawing.Size(90, 45);
+            this.btnc2l2.TabIndex = 4;
+            this.btnc2l2.UseVisualStyleBackColor = true;
+            this.btnc2l2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc2l1
+            // 
+            this.btnc2l1.Enabled = false;
+            this.btnc2l1.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc2l1.Location = new System.Drawing.Point(267, 89);
+            this.btnc2l1.Name = "btnc2l1";
+            this.btnc2l1.Size = new System.Drawing.Size(90, 45);
+            this.btnc2l1.TabIndex = 5;
+            this.btnc2l1.UseVisualStyleBackColor = true;
+            this.btnc2l1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc3l3
+            // 
+            this.btnc3l3.Enabled = false;
+            this.btnc3l3.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc3l3.Location = new System.Drawing.Point(375, 228);
+            this.btnc3l3.Name = "btnc3l3";
+            this.btnc3l3.Size = new System.Drawing.Size(90, 45);
+            this.btnc3l3.TabIndex = 6;
+            this.btnc3l3.UseVisualStyleBackColor = true;
+            this.btnc3l3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc3l2
+            // 
+            this.btnc3l2.Enabled = false;
+            this.btnc3l2.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc3l2.Location = new System.Drawing.Point(375, 160);
+            this.btnc3l2.Name = "btnc3l2";
+            this.btnc3l2.Size = new System.Drawing.Size(90, 45);
+            this.btnc3l2.TabIndex = 7;
+            this.btnc3l2.UseVisualStyleBackColor = true;
+            this.btnc3l2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnc3l1
+            // 
+            this.btnc3l1.Enabled = false;
+            this.btnc3l1.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnc3l1.Location = new System.Drawing.Point(375, 89);
+            this.btnc3l1.Name = "btnc3l1";
+            this.btnc3l1.Size = new System.Drawing.Size(90, 45);
+            this.btnc3l1.TabIndex = 8;
+            this.btnc3l1.UseVisualStyleBackColor = true;
+            this.btnc3l1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnX
+            // 
+            this.btnX.Location = new System.Drawing.Point(26, 12);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(75, 23);
+            this.btnX.TabIndex = 9;
+            this.btnX.Text = "X começa";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // btnO
+            // 
+            this.btnO.Location = new System.Drawing.Point(26, 50);
+            this.btnO.Name = "btnO";
+            this.btnO.Size = new System.Drawing.Size(75, 23);
+            this.btnO.TabIndex = 10;
+            this.btnO.Text = "O começa";
+            this.btnO.UseVisualStyleBackColor = true;
+            this.btnO.Click += new System.EventHandler(this.btnO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(149, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Vez Do:";
+            // 
+            // lblVez
+            // 
+            this.lblVez.AutoSize = true;
+            this.lblVez.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblVez.Location = new System.Drawing.Point(249, 21);
+            this.lblVez.Name = "lblVez";
+            this.lblVez.Size = new System.Drawing.Size(20, 27);
+            this.lblVez.TabIndex = 12;
+            this.lblVez.Text = "-";
+            // 
+            // btnRecomecar
+            // 
+            this.btnRecomecar.Location = new System.Drawing.Point(26, 89);
+            this.btnRecomecar.Name = "btnRecomecar";
+            this.btnRecomecar.Size = new System.Drawing.Size(75, 23);
+            this.btnRecomecar.TabIndex = 13;
+            this.btnRecomecar.Text = "Recomeçar";
+            this.btnRecomecar.UseVisualStyleBackColor = true;
+            this.btnRecomecar.Click += new System.EventHandler(this.btnRecomecar_Click);
+            // 
+            // lblVencedor
+            // 
+            this.lblVencedor.AutoSize = true;
+            this.lblVencedor.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblVencedor.Location = new System.Drawing.Point(316, 313);
+            this.lblVencedor.Name = "lblVencedor";
+            this.lblVencedor.Size = new System.Drawing.Size(20, 27);
+            this.lblVencedor.TabIndex = 15;
+            this.lblVencedor.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(125, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "O Vencedor é o:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.Controls.Add(this.lblVencedor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRecomecar);
+            this.Controls.Add(this.lblVez);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnO);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnc3l1);
+            this.Controls.Add(this.btnc3l2);
+            this.Controls.Add(this.btnc3l3);
+            this.Controls.Add(this.btnc2l1);
+            this.Controls.Add(this.btnc2l2);
+            this.Controls.Add(this.btnc2l3);
+            this.Controls.Add(this.btnc1l3);
+            this.Controls.Add(this.btnc1l2);
+            this.Controls.Add(this.btnc1l1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnc1l1;
+        private System.Windows.Forms.Button btnc1l2;
+        private System.Windows.Forms.Button btnc1l3;
+        private System.Windows.Forms.Button btnc2l3;
+        private System.Windows.Forms.Button btnc2l2;
+        private System.Windows.Forms.Button btnc2l1;
+        private System.Windows.Forms.Button btnc3l3;
+        private System.Windows.Forms.Button btnc3l2;
+        private System.Windows.Forms.Button btnc3l1;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVez;
+        private System.Windows.Forms.Button btnRecomecar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblVencedor;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinformJogoVElha/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformJogoVElha
+{
+    
+    public partial class Form1 : Form
+    {
+        bool vezX = false;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            vezX = true;
+            lblVez.Text = "X";
+            botoesEnabel();
+            btnX.Enabled = false;
+            btnO.Enabled = false;
+        }
+        private void btnO_Click(object sender, EventArgs e)
+        {
+            vezX = false;
+            lblVez.Text = "O";
+            botoesEnabel();
+            btnX.Enabled = false;
+            btnO.Enabled = false;
+        }
+
+        private void btnRecomecar_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            btnc1l1.Enabled = false;
+            btnc2l1.Enabled = false;
+            btnc3l1.Enabled = false;
+            btnc1l2.Enabled = false;
+            btnc2l2.Enabled = false;
+            btnc3l2.Enabled = false;
+            btnc1l3.Enabled = false;
+            btnc2l3.Enabled = false;
+            btnc3l3.Enabled = false;
+
+            btnX.Enabled = true;
+            btnO.Enabled = true;
+
+            btnc1l1.Text= "";
+            btnc2l1.Text= "";
+            btnc3l1.Text= "";
+            btnc1l2.Text= "";
+            btnc2l2.Text= "";
+            btnc3l2.Text= "";
+            btnc1l3.Text= "";
+            btnc2l3.Text= "";
+            btnc3l3.Text= "";
+
+            lblVez.Text = " - ";
+            lblVencedor.Text = " - ";
+
+        }
+
+        private void botoesEnabel()
+        {
+            btnc1l1.Enabled = true;
+            btnc2l1.Enabled = true;
+            btnc3l1.Enabled = true;
+            btnc1l2.Enabled = true;
+            btnc2l2.Enabled = true;
+            btnc3l2.Enabled = true;
+            btnc1l3.Enabled = true;
+            btnc2l3.Enabled = true;
+            btnc3l3.Enabled = true;
+        }
+        private void botoesEnabelF()
+        {
+            btnc1l1.Enabled =false;
+            btnc2l1.Enabled =false;
+            btnc3l1.Enabled =false;
+            btnc1l2.Enabled =false;
+            btnc2l2.Enabled =false;
+            btnc3l2.Enabled =false;
+            btnc1l3.Enabled =false;
+            btnc2l3.Enabled =false;
+            btnc3l3.Enabled =false;
+            lblVez.Text = " - ";
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+           i++;
+           Button btn = (Button)sender;
+           if(vezX)
+            {
+                btn.Text = "X";
+                lblVez.Text = "O";
+
+            }
+            else
+            {
+                btn.Text = "O";
+                lblVez.Text = "X";
+            }
+            btn.Enabled = false;
+            verificaVencedor();
+            vezX = !vezX;                
+        }
+        private void verificaVencedor()
+        {
+            if ((btnc1l1.Text == btnc2l1.Text && btnc1l1.Text == btnc3l1.Text && btnc1l1.Text != "")||
+                (btnc1l2.Text == btnc2l2.Text && btnc1l2.Text == btnc3l2.Text && btnc1l2.Text != "")||
+                (btnc1l3.Text == btnc2l3.Text && btnc1l3.Text == btnc3l3.Text && btnc1l3.Text != "")||
+                (btnc1l1.Text == btnc1l2.Text && btnc1l1.Text == btnc1l3.Text && btnc1l1.Text != "")||
+                (btnc2l1.Text == btnc2l2.Text && btnc2l1.Text == btnc2l3.Text && btnc2l1.Text != "")||
+                (btnc3l1.Text == btnc3l2.Text && btnc3l1.Text == btnc3l3.Text && btnc3l1.Text != "")||
+                (btnc1l1.Text == btnc2l2.Text && btnc2l2.Text == btnc3l3.Text && btnc2l2.Text != "")||
+                (btnc3l1.Text == btnc2l2.Text && btnc1l3.Text == btnc2l2.Text && btnc3l1.Text != ""))
+            {
+                botoesEnabelF();
+                switch (vezX)
+                {
+                    case true:
+                        lblVencedor.Text = "X";
+                        break;
+                    case false:
+                        lblVencedor.Text = "O";
+                        
+                        break;
+                }
+            }
+            else if (i == 9)
+            {
+                lblVencedor.Text = "Empate";
+                botoesEnabelF();
+            }
+          
+        }
+    }
+
+   
+}
